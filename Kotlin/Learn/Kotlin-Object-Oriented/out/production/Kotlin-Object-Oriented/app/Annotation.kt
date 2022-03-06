3f8ada70472e3a012e0c97024b06f782ee024902
Reihannudin <78741486+Reihannudin@users.noreply.github.com>
2022-03-06 21:18:24 +0700

    Delete Kotlin/Learn/Kotlin-Object-Oriented/out/production/Kotlin-Object-Oriented/app directory

--- Kotlin/Learn/Kotlin-Object-Oriented/out/production/Kotlin-Object-Oriented/app/Annotation.kt
+++
@@ -1,8 +0,0 @@
-package app
-
-import program.MyApplication
-
-fun main() {
-    val app = MyApplication("Samsan",1)
-    println(app.info())
-}
--- Kotlin/Learn/Kotlin-Object-Oriented/out/production/Kotlin-Object-Oriented/app/InlineClass.kt
+++
@@ -1,9 +0,0 @@
-package app
-
-import data.Token
-
-fun main() {
-    val token = Token("token")
-    println(token.value)
-    println(token.toUpper())
-}
